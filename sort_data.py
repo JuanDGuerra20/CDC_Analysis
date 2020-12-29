0f86b71e00935a82b5d3a7bf17c274ff43a2fb38
Juan David Guerra <juandavidguerra1@gmail.com>
2020-12-29 15:20:38 -0500

    Fixed an issue where the data types weren't being converted to the proper types

--- sort_data.py
+++ sort_data.py
@@ -4,6 +4,8 @@ Start Date: December 28, 2020
 Outline: This module will hold the classes and methods needed to organize the data so we can easily access it
 """
 
+import doctest
+
 
 class DiseaseTypeArea:
     """
@@ -37,6 +39,33 @@ class DiseaseTypeArea:
         self.incidence_data = {}
         self.mortality_data = {}
 
+        # making sure that the parameters are the proper type
+        try:
+            age_adjusted_rate = float(age_adjusted_rate)
+
+        except:
+            raise TypeError("Incorrect data type: age adjusted rate must be a float")
+
+        try:
+            count = int(count)
+        except:
+            raise TypeError("Incorrect data type: count must be an int")
+
+        try:
+            population = int(population)
+        except:
+            raise TypeError("Incorrect data type: population must be an int")
+
+        try:
+            data_year = int(data_year)
+        except:
+            raise TypeError("Incorrect data type: data_year must be an int")
+
+        try:
+            crude_rate = float(crude_rate)
+        except:
+            raise TypeError("Incorrect data type: crude rate must be a float")
+
         # we sort the types of data by the parameter count_type which will either be incidence or mortality
         if count_type == "Incidence":
             # adding all the desired information to the incidence data dictionary at the specific year
@@ -49,6 +78,10 @@ class DiseaseTypeArea:
             self.mortality_data[data_year] = {'count': count, 'population': population,
                                               'age adjusted rate': age_adjusted_rate, 'crude rate': crude_rate,
                                               'race': race, 'sex': sex}
+        # if the count_type is not Incidence or Mortality, we want to raise a value error since those are the only types
+        # of count_types we are taking into account for this type of disease
+        else:
+            raise ValueError("Unsupported count_type: must be either Incidence or Mortality")
 
     def __str__(self):
         """
@@ -66,6 +99,14 @@ class DiseaseTypeArea:
         to the appropriate instance attributes
         :param data: String
         :return: None
+        Example:
+        >>> area = DiseaseTypeArea('Alabama', 'All Cancer Sites Combined', 367.2, 9299, 'Incidence', 2293259,\
+        "All Races", 'Female', 1999, 405.5)
+        >>> area.add_yearly_data('Alabama|359.7|374.7|367.2|9299|Incidence|2293259|All Races|Female|All Cancer Sites Combined|2000|397.3|413.8|405.5')
+        >>> print(area)
+        Disease: All Cancer Sites Combined	Area: Alabama
+        Incidence data: {1999: {'count': 9299, 'population': 2293259, 'age adjusted rate': 367.2, 'crude rate': 405.5, 'race': 'All Races', 'sex': 'Female'}, 2000: {'count': 9299, 'population': 2293259, 'age adjusted rate': 367.2, 'crude rate': 405.5, 'race': 'All Races', 'sex': 'Female'}}
+        Mortality data: {}
         """
 
         # splitting the input data at the delimiter (|)
@@ -74,33 +115,34 @@ class DiseaseTypeArea:
         # assigning the desired data types to the proper variables and making sure they are the correct type
         age_adjusted_rate = data[3]
         try:
-            float(age_adjusted_rate)
+            age_adjusted_rate = float(age_adjusted_rate)
 
         except:
             raise TypeError("Incorrect data type: age adjusted rate must be a float")
 
         count = data[4]
         try:
-            int(count)
+            count = int(count)
         except:
             raise TypeError("Incorrect data type: count must be an int")
 
         population = data[6]
         try:
-            int(population)
+            population = int(population)
         except:
             raise TypeError("Incorrect data type: population must be an int")
 
         data_year = data[10]
         try:
-            int(data_year)
+            data_year = int(data_year)
 
         except:
             raise TypeError("Incorrect data type: data_year must be an int")
 
         crude_rate = data[13]
         try:
-            float(crude_rate)
+            crude_rate = float(crude_rate)
+
         except:
             raise TypeError("Incorrect data type: crude rate must be a float")
 
@@ -128,3 +170,6 @@ class DiseaseTypeArea:
 
 alabama_all = DiseaseTypeArea('Alabama', 'All Cancer Sites Combined', 367.2, 9299, 'Incidence', 2293259, "All Races",
                               'Female', 1999, 405.5)
+
+if __name__ == '__main__':
+    doctest.testmod()
